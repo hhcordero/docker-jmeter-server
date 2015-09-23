e12e25699b99a455bb32f59a3a07cec8ff6abecd
Hector Cordero <hhcordero@gmail.com>
2015-09-23 00:56:44 +0000

    Set hostname by passing as parameter instead of via env variable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ s=$(($freeMem/10*8))
 x=$(($freeMem/10*8))
 n=$(($freeMem/10*2))
 export JVM_ARGS="-Xmn${n}m -Xms${s}m -Xmx${x}m"
-export RMI_HOST_DEF=-Djava.rmi.server.hostname=$IP
 JMETER_LOG="jmeter-server.log" && touch $JMETER_LOG && tail -f $JMETER_LOG &
 exec jmeter-server \
+    -D "java.rmi.server.hostname=${IP}" \
     -D "client.rmi.localport=${RMI_PORT}" \
     -D "server.rmi.localport=${RMI_PORT}"
